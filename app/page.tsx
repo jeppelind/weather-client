8d7433e2ee7f20d8d0d2b2485e5590ba4f0cb26a
jeppelind <58559776+jeppelind@users.noreply.github.com>
2025-01-07 15:23:56 +0100

    Basic search functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { WiThermometer } from "react-icons/wi";
 import WeatherIcon from "./_components/weather-icon";
+import Search from "./Search";
 
 type Forecast = {
   location: {
@@ -45,36 +46,28 @@ type Forecast = {
   },
 }
 
-type SearchParams = {
-  location: string,
+type WeatherDisplayProps = {
+  data: Forecast,
   temp: string,
 }
 
 const getForecast = async (location: string) => {
   const URL = `${process.env.API_URL}/forecast?location=${location}`;
-  try {
-    const res = await fetch(URL, {
-      next: {
-        revalidate: 3600, // Cache age in seconds
-      }
-    });
-    if (!res.ok) {
-      throw new Error(`Error fetching data. ${res.statusText}`);
+  const res = await fetch(URL, {
+    next: {
+      revalidate: 3600, // Cache age in seconds
     }
-    const data: Forecast = await res.json();
-    return data;
-  } catch (err) {
-    console.error(err)
-    return null;
+  });
+  if (!res.ok) {
+    throw new Error(`Error fetching data. ${res.statusText}`);
   }
+  console.log(res);
+  const data: Forecast = await res.json();
+  console.log(data);
+  return data;
 }
 
-const CurrentWeather = async ({ location, temp }: SearchParams) => {
-  const data = await getForecast(location);
-  if (!data) {
-    return <p className="text-xl">{`Problem fethching data for "${location}"`}</p>;
-  }
-
+const CurrentWeather = async ({ data, temp }: WeatherDisplayProps) => {
   return (
     <>
       <p>{data.location.name}, {data.location.country}</p>
@@ -92,12 +85,7 @@ const CurrentWeather = async ({ location, temp }: SearchParams) => {
   )
 }
 
-const HourlyForecast = async ({ location, temp }: SearchParams) => {
-  const data = await getForecast(location);
-  if (!data) {
-    return null;
-  }
-
+const HourlyForecast = async ({ data, temp }: WeatherDisplayProps) => {
   const elementArr: JSX.Element[] = [];
   const timeNow = Date.now();
   const startIdx = data.forecast.hours.findIndex(hour => hour.time_epoch > timeNow);
@@ -125,12 +113,7 @@ const HourlyForecast = async ({ location, temp }: SearchParams) => {
   );
 }
 
-const DailyForecast = async ({ location, temp }: SearchParams) => {
-  const data = await getForecast(location);
-  if (!data) {
-    return null;
-  }
-  
+const DailyForecast = async ({ data, temp }: WeatherDisplayProps) => {
   const getDayLabel = (dateStr: string) => {
     const dayLabels = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const date = new Date(dateStr);
@@ -172,26 +155,28 @@ export default async function Home({
   
   if (location) {
     try {
+      const data = await getForecast(location);
       return (
         <main className="flex min-h-screen flex-col items-center pt-16">
-          <CurrentWeather location={location} temp={temp} />
+        <CurrentWeather data={data} temp={temp} />
 
-          <div className="mt-16 px-10 min-w-full sm:min-w-128">
-            <HourlyForecast location={location} temp={temp} />
-          </div>
+        <div className="mt-16 px-10 min-w-full sm:min-w-128">
+          <HourlyForecast data={data} temp={temp} />
+        </div>
 
-          <div className="mt-8 px-10 min-w-full sm:min-w-128">
-            <DailyForecast location={location} temp={temp} />
-          </div>
-        </main>
+        <div className="mt-8 px-10 min-w-full sm:min-w-128">
+          <DailyForecast data={data} temp={temp} />
+        </div>
+      </main>
       )
     } catch (err) {
       console.error(err)
       return (
-        <main className="flex items-center flex-col">
-          <div className="flex min-h-screen flex-col justify-center items-center">
-            <WiThermometer className="text-7xl" />
-            <p>Problem fetching data</p>
+        <main className="flex min-h-screen flex-col items-center justify-center">
+          <div className="flex flex-col items-center">
+            <p className="text-xl">{`Problem fetching data for "${location}"`}</p>
+            <p className="text-lg mb-12">Please try again.</p>
+            <Search />
           </div>
         </main>
       )
@@ -199,13 +184,10 @@ export default async function Home({
   }
 
   return (
-    <main>
-      <main className="flex items-center flex-col">
-          <div className="flex min-h-screen flex-col justify-center items-center">
-            <p className="text-lg">Simple Weather</p>
-            <p>Please provide a location to show forecast.</p>
-          </div>
-        </main>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <Search />
+      </div>
     </main>
   )
 }
